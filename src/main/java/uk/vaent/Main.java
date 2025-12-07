f029d8e8eb3345cb8f24b7f81a14047c642ae9c0
Alexander Kriegisch <Alexander@Kriegisch.name>
2025-12-07 10:02:49 +0700

    Update main method to use controller and trigger aspect

--- src/main/java/uk/vaent/Main.java
+++ src/main/java/uk/vaent/Main.java
@@ -3,16 +3,22 @@ package uk.vaent;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import uk.vaent.controller.MyController;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder()
+        try (
+            ConfigurableApplicationContext appContext = new SpringApplicationBuilder()
                 .bannerMode(Banner.Mode.OFF)
                 .logStartupInfo(true)
                 .sources(Main.class)
-                .run(args);
+                .run(args)) {
+            // Call controller to trigger aspect for demonstration
+            appContext.getBean(MyController.class).getCount();
+        }
     }
 
 }
